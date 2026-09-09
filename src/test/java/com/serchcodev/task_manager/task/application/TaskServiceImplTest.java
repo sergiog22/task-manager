968eb5a45cc9e9cbe679bbaa5e21af222e8740d7
sergiog22 <serg.garcia.2209@gmail.com>
2026-09-08 18:00:48 -0600

    Update POM packaging type to jar and remove unused import in TaskServiceImplTest

--- src/test/java/com/serchcodev/task_manager/task/application/TaskServiceImplTest.java
+++ src/test/java/com/serchcodev/task_manager/task/application/TaskServiceImplTest.java
@@ -21,7 +21,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
